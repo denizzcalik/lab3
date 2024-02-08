6e2523a0e608e12bb5682c46e890f54fbfcb5c29
ziggesommar <zigge@chalmers.se>
2024-02-08 09:18:54 +0100

    Changes in CarView, DrawPanel and CarController classes

--- Lab3/main/java/CarController.java
+++ Lab3/main/java/CarController.java
@@ -32,9 +32,9 @@ public class CarController {
         CarController cc = new CarController();
 
         //
-        cc.cars.add(new Volvo240(Color.blue, 12, 5));
-        cc.cars.add(new Saab95(Color.blue, 12, 105));
-        cc.cars.add(new Scania(Color.blue, 12, 205));
+        cc.cars.add(new Volvo240(Color.blue, 300, 5));
+        cc.cars.add(new Saab95(Color.blue, 300, 105));
+        cc.cars.add(new Scania(Color.blue, 300, 205));
 
         // Start a new view and send a reference of self
         cc.frame = new CarView("CarSim 1.0", cc);
@@ -44,8 +44,8 @@ public class CarController {
     }
 
     /* Each step the TimerListener moves all the cars in the list and tells the
-    * view to update its images. Change this method to your needs.
-    * */
+     * view to update its images. Change this method to your needs.
+     * */
     private class TimerListener implements ActionListener {
         public void actionPerformed(ActionEvent e) {
             for (Car car : cars) {
@@ -53,12 +53,12 @@ public class CarController {
                 int x = (int) Math.round(car.getX());
                 int y = (int) Math.round(car.getY());
 
-                if (x <= 0 || x >= frame.drawPanel.getWidth() - 70){
+                if (x < 0 || x >= frame.drawPanel.getWidth() - 70) {
                     car.turnLeft();
                     car.turnLeft();
                 }
 
-                if (y <= 0 || y >= frame.drawPanel.getHeight() - 70){
+                if (y <= 0 || y >= frame.drawPanel.getHeight() - 70) {
                     car.turnLeft();
                     car.turnLeft();
                 }
@@ -73,7 +73,7 @@ public class CarController {
     void gas(int amount) {
         double gas = ((double) amount) / 100;
         for (Car car : cars
-                ) {
+        ) {
             car.gas(gas);
         }
     }
@@ -85,4 +85,64 @@ public class CarController {
             car.brake(brake);
         }
     }
-}
+    protected void turboOn() {
+        for (Car car : cars) {
+            if (car instanceof Saab95) {
+                ((Saab95) car).turbocar.setTurboOn();
+            }
+        }
+    }
+    protected void turboOff(){
+        for (Car car : cars) {
+            if (car instanceof Saab95) {
+                ((Saab95) car).turbocar.setTurboOff();
+            }
+        }
+    }
+    protected void raiseScaniaPlatform(){
+        for (Car car : cars) {
+            if (car instanceof Scania) {
+                ((Scania) car).raisePlatform();
+            }
+        }
+    }
+
+    protected void lowerScaniaPlatform(){
+        for (Car car : cars) {
+            if (car instanceof Scania) {
+                ((Scania) car).lowerPlatform();
+            }
+        }
+    }
+
+    protected void stopAllCars(){
+        for (Car car : cars) {
+            car.stopEngine();
+        }
+    }
+
+    protected void startAllCars(){
+        for (Car car : cars) {
+            car.startEngine();
+        }
+    }
+    protected void VolvoCarShopCollision(){
+        for (Car car : cars){
+            double x = car.getX();
+            double y = car.getY();
+            if (frame.drawPanel.VolvoWorkshop.getX() - 10 < x && x < frame.drawPanel.VolvoWorkshop.getX() + 10
+                    && frame.drawPanel.VolvoWorkshop.getY() - 10 < y && y < frame.drawPanel.VolvoWorkshop.getY() + 10){
+                if (car instanceof Volvo240){
+                    frame.drawPanel.VolvoWorkshop.load((Volvo240)car);
+                    car.stopEngine();
+                }
+                else {
+                    car.turnLeft();
+                }
+
+        }
+    }
+
+}}
+
+
